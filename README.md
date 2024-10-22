21a02267a420892498b3f3890384ae377c59972a
HenryLHH <haohongl@andrew.cmu.edu>
2024-10-22 13:26:55 -0700

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
-# fusion
+## FUSION: Safety-aware Causal Representation for Trustworthy Offline Reinforcement Learning in Autonomous Driving
 
-### Install Guidelines
+#### [[Project Page]](https://sites.google.com/view/safe-fusion/) [[RAL Paper]](https://ieeexplore.ieee.org/document/10476686) [[Arxiv Preprint]](https://arxiv.org/pdf/2311.10747) [[Dataset]](https://drive.google.com/drive/folders/10T-i_SlHRkB5FKLCa1BO4rpZgw-9x3AN) [[Video]](https://drive.google.com/file/d/1lV1NWw0D1nH1-KjX1RqH4kDcaFSIJM1i/view)
+
+
+### 🔍 Structure
+The core structure of this repo is as follows:
+```
+├── fusion
+│   ├── agent       # the training configs of each algorithm
+│   ├── configs     # the evaluation escipts
+│   ├── envs        # the training and testing environments
+├── utils           # the globally shared utility functions
+├── train           # the training scripts
+├── tools           # tools for debugging and visualization
+├── scripts
+```
+The implemented offline safe RL and imitation learning algorithms include:
+
+| Algorithm           | Type           | Description           |
+|:-------------------:|:-----------------:|:------------------------:|
+| BC               | Imitation Learning | [Behavior Cloning](https://arxiv.org/abs/2302.07351) |
+| ICIL            | Imitation Learning           | [Invariant Causal Imitation Learning](https://arxiv.org/pdf/1812.02900.pdf)|
+| GSA            | Imitation Learning           | [Generalized State Abstraction](https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=8594201) |
+| BEAR-Lag            | Q-learning           | [BEARL](https://arxiv.org/abs/1906.00949) with [PID Lagrangian](https://arxiv.org/abs/2007.03964)   |
+| BCQ-Lag             | Q-learning           | [BCQ](https://arxiv.org/pdf/1812.02900.pdf) with [PID Lagrangian](https://arxiv.org/abs/2007.03964) |
+| CPQ                 | Q-learning           | [Constraints Penalized Q-learning (CPQ)](https://arxiv.org/abs/2107.09003) |
+
+
+### 📝 Guidelines
+
+#### Installation
 
 ```shell
 git clone https://github.com/HenryLHH/fusion
@@ -10,14 +39,43 @@ cd fusion
 conda create -n fusion
 conda activate fusion
 pip install -e .[all]
-
-# train the agents
-bash run_fusion.sh
 ```
 
+#### Training and Evaluation
+
+```shell
+# train the FUSION agents
+bash scripts/run_fusion.sh
+# train the other agents
+bash scripts/run_all.sh
+# evaluate the trained model
+bash scripts/run_eval_task.sh
+# visualization
+bash scripts/run_vis.sh
+```
 ------------
 
-### Data Availability
+### 💾 Data Availability
 
 Our dataset to train the offline RL is available on this [Google Drive Link](https://drive.google.com/drive/folders/10T-i_SlHRkB5FKLCa1BO4rpZgw-9x3AN?usp=sharing). 
 
+### ❤️ Acknowledgement 
+
+We acknowledge the following related repositories which contributes to some of our baselines in this offline RL libraries for autonomous driving in metadrive:
+
+- ICIL: https://github.com/ioanabica/Invariant-Causal-Imitation-Learning
+- OSRL: https://github.com/liuzuxin/OSRL
+
+### 📚 Reference
+
+For more information about implementation, you are welcome to check our [RAL paper](https://arxiv.org/pdf/2311.10747): 
+
+```bibtex
+@article{lin2024safety,
+  title={Safety-aware causal representation for trustworthy offline reinforcement learning in autonomous driving},
+  author={Lin, Haohong and Ding, Wenhao and Liu, Zuxin and Niu, Yaru and Zhu, Jiacheng and Niu, Yuming and Zhao, Ding},
+  journal={IEEE Robotics and Automation Letters},
+  year={2024},
+  publisher={IEEE}
+}
+```
--- scripts/run_eval_task.sh
+++ scripts/run_eval_task.sh
@@ -7,25 +7,25 @@ do
     #         --single_env --ep_num 50 --task $task # --generalize  
     # python tools/6_eval_checkpoint.py --method ssr --model context_20_single --context 20 \
     #     --horizon 1000 --cost 20.0 --single_env --ep_num 50 # --generalize
-#     python tools/6_eval_checkpoint_task.py --method ssr --model context_20_single_dynamics_value --context 20 \
-#             --horizon 1000 --cost 5.0  --dynamics --value --single_env --ep_num 50 --task $task --state_pred  --causal # --generalize
-#     python tools/6_eval_checkpoint.py --method gsa --model gsa_single --context 20 \
-#             --horizon 1000 --cost 5.0 --single_env --ep_num 50 --task $task # --generalize
-#     python tools/6_eval_checkpoint_task.py --method icil --model icil_state_single \
-#             --horizon 1000 --cost 5.0 --dynamics --value --single_env --task $task # --state_pred --causal # --generalize # --causal # --generalize
-#     python tools/6_eval_checkpoint_task.py --method bnn --model bnn_single \
-#             --horizon 1000 --cost 5.0 --dynamics --value --single_env --task $task # --generalize
-#     python tools/6_eval_checkpoint_task.py --method bc --model bc_state_single_ \
-#             --horizon 1000 --cost 5.0 --dynamics --value --single_env --task $task # --generalize 
-#     python tools/6_eval_checkpoint_task.py --method bcql  \
-#             --model "logs/MetaDrive-TopDown-v0-cost-1/BCQL_cost1_datasetdataset_mixed_single_post_single_envTrue-f05d/BCQL_cost1_datasetdataset_mixed_single_post_single_envTrue-f05d/checkpoint/model_best.pt" \
-#             --horizon 1000 --cost 5.0 --dynamics --value --single_env  --ep_num 50 --task $task # --generalize    
-#     python tools/6_eval_checkpoint_task.py --method bearl  \
-#         --model "logs/MetaDrive-TopDown-v0-cost-1/BEARL_cost1_datasetdataset_mixed_single_post_eval_every2500_single_envTrue-ab87/BEARL_cost1_datasetdataset_mixed_single_post_eval_every2500_single_envTrue-ab87/checkpoint/model.pt" \
-#         --horizon 1000 --cost 5.0 --dynamics --value --single_env --ep_num 50 --task $task # --generalize        
-#     python tools/6_eval_checkpoint_task.py --method cpq  \
-#             --model "logs/MetaDrive-TopDown-v0-cost-1/CPQ_cost1_datasetdataset_mixed_single_post_single_envTrue-2432/CPQ_cost1_datasetdataset_mixed_single_post_single_envTrue-2432/checkpoint/model_best.pt" \
-#             --horizon 1000 --cost 5.0 --dynamics --value --single_env --ep_num 50 --task $task # --generalize
+    # python tools/6_eval_checkpoint_task.py --method ssr --model context_20_single_dynamics_value --context 20 \
+    #         --horizon 1000 --cost 5.0  --dynamics --value --single_env --ep_num 50 --task $task --state_pred  --causal # --generalize
+    # python tools/6_eval_checkpoint.py --method gsa --model gsa_single --context 20 \
+    #         --horizon 1000 --cost 5.0 --single_env --ep_num 50 --task $task # --generalize
+    # python tools/6_eval_checkpoint_task.py --method icil --model icil_state_single \
+    #         --horizon 1000 --cost 5.0 --dynamics --value --single_env --task $task # --state_pred --causal # --generalize # --causal # --generalize
+    # python tools/6_eval_checkpoint_task.py --method bnn --model bnn_single \
+    #         --horizon 1000 --cost 5.0 --dynamics --value --single_env --task $task # --generalize
+    # python tools/6_eval_checkpoint_task.py --method bc --model bc_state_single_ \
+    #         --horizon 1000 --cost 5.0 --dynamics --value --single_env --task $task # --generalize 
+    # python tools/6_eval_checkpoint_task.py --method bcql  \
+    #         --model "logs/MetaDrive-TopDown-v0-cost-1/BCQL_cost1_datasetdataset_mixed_single_post_single_envTrue-f05d/BCQL_cost1_datasetdataset_mixed_single_post_single_envTrue-f05d/checkpoint/model_best.pt" \
+    #         --horizon 1000 --cost 5.0 --dynamics --value --single_env  --ep_num 50 --task $task # --generalize    
+    # python tools/6_eval_checkpoint_task.py --method bearl  \
+    #     --model "logs/MetaDrive-TopDown-v0-cost-1/BEARL_cost1_datasetdataset_mixed_single_post_eval_every2500_single_envTrue-ab87/BEARL_cost1_datasetdataset_mixed_single_post_eval_every2500_single_envTrue-ab87/checkpoint/model.pt" \
+    #     --horizon 1000 --cost 5.0 --dynamics --value --single_env --ep_num 50 --task $task # --generalize        
+    # python tools/6_eval_checkpoint_task.py --method cpq  \
+    #         --model "logs/MetaDrive-TopDown-v0-cost-1/CPQ_cost1_datasetdataset_mixed_single_post_single_envTrue-2432/CPQ_cost1_datasetdataset_mixed_single_post_single_envTrue-2432/checkpoint/model_best.pt" \
+    #         --horizon 1000 --cost 5.0 --dynamics --value --single_env --ep_num 50 --task $task # --generalize
 done
 
 
